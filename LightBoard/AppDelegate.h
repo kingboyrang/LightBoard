d6e7b8a5e16fd7e1761975ee12d64cedef555908
kingboyrang <zhongduan-mini@zhongduan-minideMac-mini.local>
2015-01-19 09:16:27 +0800

    Initial Commit

---
+++ LightBoard/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LightBoard
+//
+//  Created by wulanzhou-mini on 15-1-19.
+//  Copyright (c) 2015年 wulanzhou-mini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LightBoard/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LightBoard
+//
+//  Created by wulanzhou-mini on 15-1-19.
+//  Copyright (c) 2015年 wulanzhou-mini. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
